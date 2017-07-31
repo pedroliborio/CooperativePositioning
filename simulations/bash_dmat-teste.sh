11f1ccc83fed10681f1993d25bc6d210f17b2527
pedroliborio <pedroliborioln@gmail.com>
2017-07-31 15:39:11 -0300

    To Simulate

--- simulations/bash_dmat-teste.sh
+++ simulations/bash_dmat-teste.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-for seed in {0..1}; #trafego CBR
+for seed in {0..30}; #trafego CBR
 do
 	../CooperativePositioning -u Cmdenv -c DMAT -n ..:../../veins/examples/veins:../../veins/src/veins --image-path=../../veins/images -l ../../veins/src/veins --debug-on-errors=false omnetpp.ini --num-rngs=10 --repeat=1 --seed-set=$seed
-	#../localization/GPS/outagesxy/restore_counterfiles.py
-
 done
